2eb4873184cef77d2ac6e89a699cad77573091bc
Ramiro Bou <bouramiro@gmail.com>
2014-01-27 19:08:12 -0300

    Añadiendo modulo google.

---
+++ modules/google/google.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import json
+import urllib.request
+
+
+class google:
+
+    def __init__(self, core, client):
+        try:
+            self.apikey = core.mconf['moduleconf']['googleapikey']
+            if self.apikey == "":
+                return None
+        except:
+            return None
+        core.addCommandHandler("google", self, chelp=
+        "Realiza una busqueda en google. Sintaxis: google <texto>")
+
+    def google(self, bot, cli, event):
+        if len(event.splitd) > 0:
+            stext = " ".join(event.splitd)
+        else:
+            cli.privmsg(event.target, "\00304Error\003: Faltan parametros")
+        r = urllib.request.urlopen("https://www.googleapis.com/customsearch/v1?"
+         "num=3&key=%s&cx=001206920739550302428:fozo2qblwzc&q=%s&alt=json" %
+         (self.apikey, stext)).read()
+
+        search = json.loads(r.decode('utf-8'))
+
+        resp = "Resultados de la búsqueda en Google de " + \
+        " \"\2%s\2\": " % stext + "\2%s\2 \037\00310%s\003\037, \2%s\2" \
+        " \037\00310%s\003\037" % (search['items'][0]['title'],
+        search['items'][0]['link'], search['items'][1]['title'],
+        search['items'][1]['link']) + ", \2%s\2 \037\00310%s\003\037" % \
+        (search['items'][2]['title'], search['items'][2]['link'])
+
+        cli.privmsg(event.target, resp)
