cbf88e4c40c494028e92bb9a448083d634014bc1
Ramiro Bou <bouramiro@gmail.com>
2014-02-28 18:58:27 -0300

    Ahora m_raw funciona de nuevo.

--- modules/raw/raw.py
+++ modules/raw/raw.py
@@ -8,4 +8,4 @@ class raw:
         chelp="Envía información raw al servidor. Sintaxis: raw <texto>")
 
     def raw(self, bot, cli, ev):
-        cli.send_raw(" ".join(ev.splitd))
+        cli.send(" ".join(ev.splitd))
