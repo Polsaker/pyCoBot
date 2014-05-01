7b2b4488a4cf4d36eb0a3af783c0f8a21aef2aea
Ramiro Bou <bouramiro@gmail.com>
2014-05-01 02:55:44 -0300

    Definitivamente.

--- modules/translate/translate.py
+++ modules/translate/translate.py
@@ -14,7 +14,7 @@ class translate:
             cli.notice(ev.target, "\00304Error\003: Faltan parametros")
             return 0
         cli.notice(ev.target, "Traducido: {0}".format(
-            translate(" ".join(ev.splitd[2:]), ev.splitd[1], ev.splitd[0])))
+            self.translate(" ".join(ev.splitd[2:]), ev.splitd[1], ev.splitd[0])))
     
     def translate(self, text, OUT, IN):
         text = urllib.parse.quote(text)
@@ -27,14 +27,3 @@ class translate:
             if i >= 1: res[i] = res[i][1:res[i].index(b"\",\"")]
             else:      res[i] = res[i][:res[i].index(b"\",\"")]
         return res[0]
-
-    
-    def translate(to_translate, to_langage="auto", langage="auto"):
-        agents = {'User-Agent':"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"}
-        before_trans = 'class="t0">'
-        link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s" % (to_langage, langage, to_translate.replace(" ", "+"))
-        request = urllib.request.Request(link, headers=agents)
-        page = urllib.request.urlopen(request).read()
-        result = page[page.find(before_trans)+len(before_trans):]
-        result = result.split("<")[0]
-        return result
