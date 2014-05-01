c01ccdd41db8bf3afb8e1658cd4ace0966bb0801
Ramiro Bou <bouramiro@gmail.com>
2014-05-01 15:40:20 -0300

    Mejorando el traductor

--- modules/translate/translate.py
+++ modules/translate/translate.py
@@ -1,4 +1,8 @@
-import http.client, urllib.request, urllib.parse, urllib.error, string
+import http.client
+import urllib.request
+import urllib.parse
+import urllib.error
+import re
 
 
 class translate:
@@ -8,22 +12,37 @@ class translate:
         " 'de' puede ser 'auto' para autodetectar el idioma.", alias=
         ['translate', 'tr'])
 
-
     def traducir(self, bot, cli, ev):
         if len(ev.splitd) < 2:
             cli.notice(ev.target, "\00304Error\003: Faltan parametros")
             return 0
-        cli.notice(ev.target, "Traducido: {0}".format(
-            self.translate(" ".join(ev.splitd[2:]), ev.splitd[1], ev.splitd[0])))
-    
-    def translate(self, text, OUT, IN):
+        self.translate(" ".join(ev.splitd[2:]), ev.splitd[1], ev.splitd[0], cli,
+                ev.target)
+
+    def translate(self, text, OUT, IN, cli, to):
         text = urllib.parse.quote(text)
         conn = http.client.HTTPConnection("translate.google.com")
-        conn.request("GET", "/translate_a/t?client=t&text="+text+"&hl="+IN+"&tl="+OUT)
+        conn.request("GET", "/translate_a/t?client=t&text=" + text +
+                    "&hl=" + IN + "&tl=" + OUT)
         res = conn.getresponse().read()
-        res = res[4:res.index(b",\"\",\"\"]]")]
-        res = res.split(b"],[")
-        for i in range(len(res)):
-            if i >= 1: res[i] = res[i][1:res[i].index(b"\",\"")]
-            else:      res[i] = res[i][:res[i].index(b"\",\"")]
-        return str(res[0])[2:len(str(res[0])) -1]
+        p1 = re.compile("\[\[\[\"(.+)\",\"(.*)\",\"(.*)\",\"\"\]\],.*")
+        m1 = p1.search(res)
+        if m1 is not None:
+            translated = m1.group(1)
+            ftranslated = m1.group(2)
+            pronun = m1.group(3)
+            res = "Traducido: " + ftranslated + " -> " + translated
+            if pronun != "":
+                res += " (" + pronun + ")"
+        else:
+            res = "No se pudo traducir."
+
+        cli.notice(to, res)
+        #res = res[4:res.index(b",\"\",\"\"]]")]
+        #res = res.split(b"],[")
+        #for i in range(len(res)):
+            #if i >= 1:
+                #res[i] = res[i][1:res[i].index(b"\",\"")]
+            #else:
+                #res[i] = res[i][:res[i].index(b"\",\"")]
+        #return str(res[0])[2:len(str(res[0])) - 1]
