1054dcfb4efd7b9b763749f1145b73556498b463
Ramiro Bou <bouramiro@gmail.com>
2014-05-02 02:14:31 -0300

    Oops

--- pycobot/updater.py
+++ pycobot/updater.py
@@ -120,21 +120,21 @@ class pyCoUpdater:
                         potato = json.load(fp)
                         try:
                             potato['extrafiles']
+                            for kk in potato['extrafiles']:
+                                if self.processgithttp(i, "modules/" + val + "/" + kk) is True:
+                                    self.cli.privmsg(self.ev.target,
+                                     "\2Actualizando \00303%s" % "modules/" + val + "/" + kk)
+                                    self.upd = True
+                                    try:
+                                        # si esta cargado...
+                                        self.bot.modinfo[val]
+                                        # ... lo recargamos...
+                                        self.bot.unloadmod(val)
+                                        self.bot.loadmod(val, self.cli)
+                                    except:
+                                        pass  # ???
                         except KeyError:
-                            continue
-                        for kk in potato['extrafiles']:
-                            if self.processgithttp(i, "modules/" + val + "/" + kk) is True:
-                                self.cli.privmsg(self.ev.target,
-                                 "\2Actualizando \00303%s" % "modules/" + val + "/" + kk)
-                                self.upd = True
-                                try:
-                                    # si esta cargado...
-                                    self.bot.modinfo[val]
-                                    # ... lo recargamos...
-                                    self.bot.unloadmod(val)
-                                    self.bot.loadmod(val, self.cli)
-                                except:
-                                    pass  # ???
+                            pass
                                 
 
     def processgithttp(self, repo, path):
