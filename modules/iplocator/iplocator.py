3a1324a44d2a51c9bac6d5b171e757a76cb5d88b
Ramiro Bou <bouramiro@gmail.com>
2014-05-02 02:10:59 -0300

    Usando una api en vez de geoip para ubicar ips

--- modules/iplocator/iplocator.py
+++ modules/iplocator/iplocator.py
@@ -1,19 +1,48 @@
 # -*- coding: utf-8 -*-
-import .geoip2.database
-import os
-import logging
+import http.client
+import urllib.request
+import urllib.parse
+import urllib.error
+#import re
+import json
 
 
 class nickserv:
 
     def __init__(self, core, client):
-        if not os.path.exists("modules/iplocator/GeoLite2-City.mmdb"):
-            logging.error("No se ha encontrado modules/iplocator/GeoLite2-City.mmdb. Descarguelo y descomprimalo: http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz")
-            return 0
         core.addCommandHandler("ip", self, chelp=
         "Geolocaliza una ip. Sintaxis: ip <ip>")
-        self.reader = geoip2.database.Reader('modules/iplocator/GeoLite2-City.mmdb')
 
-    def ip(self, bot, cli, event):
-        response = self.reader.city('128.101.101.101')
-        cli.notice(ev.source, response.city.name)
+    def ip(self, bot, cli, ev):
+        if len(ev.splitd) < 1:
+            cli.notice(ev.target, "\00304Error\003: Faltan parametros")
+            return 0
+        text = urllib.parse.quote(ev.splitd[0])
+        conn = http.client.HTTPConnection("ip-api.com")
+        conn.request("GET", "json/{0}?fields=65535 ".format(text))
+        res = conn.getresponse().read()
+        data = json.loads(res)
+        if data.status == "success":
+            resp = "IP \2{0}\2 ".format(ev.splitd[0])
+            if data['reverse'] != "": 
+                resp += "- {0} ".format(data['reverse'])
+            if data['country'] != "":
+                resp += "\2País\2: {0} ".format(data['country'])
+            if data['region'] != "":
+                resp += "\2Región\2: {0} ".format(data['region'])
+            if data['city'] != "":
+                resp += "\2Ciudad\2: {0} ".format(data['city'])
+            if data['isp'] != "":
+                resp += "\2ISP\2: {0} ".format(data['isp'])
+            if data['org'] != "":
+                resp += "\2Organización\2: {0} ".format(data['org'])
+            if data['as'] != "":
+                resp += "\2ASN\2: {0} ".format(data['as'])
+            if data['timezone'] != "":
+                resp += "\2Zona horaria\2: {0} ".format(data['timezone'])
+            cli.notice(ev.target, resp)
+        else:
+            cli.notice(ev.target, "\00304Error\003: No se pudo procesar la IP.")
+    
+def isset(variable):
+	return variable in locals() or variable in globals()
