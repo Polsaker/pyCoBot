118145729429ea1d61193300c86f756cefd9b58a
Ramiro Bou <bouramiro@gmail.com>
2014-05-02 18:57:48 -0300

    Usando multiprocessing para limitar el tiempo en el que se procesan los calculos

--- modules/calc/calc.py
+++ modules/calc/calc.py
@@ -2,6 +2,7 @@
 import math
 import re
 import textwrap
+import multiprocessing
 
 
 class calc:
@@ -9,9 +10,14 @@ class calc:
     def __init__(self, core, client):
         core.addCommandHandler("calc", self, chelp=
         "Calculadora. Sintaxis: calc <cálculo>")
+        self.res = None
+        self.q = multiprocessing.Queue()
 
     def calc(self, bot, cli, event):
-        res = self.calculate(" ".join(event.splitd))
+        #res = self.calculate(" ".join(event.splitd))
+
+        res = self.try_slow_thing(self.calculate,
+                                        " ".join(event.splitd), self.q)
         if res is None:
             cli.notice(event.target, "No se pudo calcular.")
         else:
@@ -20,11 +26,21 @@ class calc:
 
     integers_regex = re.compile(r'\b[\d\.]+\b')
 
-    def calculate(self, expr):
+    def calculate(self, expr, q):
         def safe_eval(expr, symbols={}):
             if expr.find("_") != -1:
                 return None
             return eval(expr, dict(__builtins__=None), symbols)  # :(
 
         expr = expr.replace('^', '**')
-        return safe_eval(expr, vars(math))
+        q.put(safe_eval(expr, vars(math)))
+
+    def try_slow_thing(self, function, *args):
+        p = multiprocessing.Process(target=function, args=args)
+        p.start()
+        p.join(5)
+        if p.is_alive():
+            p.terminate()
+            return "La operación se ha demorado mucho en finalizar"
+        else:
+            return self.q.get()
