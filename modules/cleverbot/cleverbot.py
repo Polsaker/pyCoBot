9155c8af43f2319ce62338da5d4e5616ca9a7f38
Ramiro Bou <bouramiro@gmail.com>
2014-05-12 22:46:40 -0300

    Utilizando una libreria distinta para cleverbot.

--- modules/cleverbot/cleverbot.py
+++ modules/cleverbot/cleverbot.py
@@ -1,7 +1,3 @@
-import urllib.request
-import hashlib
-import re
-import html.parser
 import time
 
 
@@ -10,6 +6,8 @@ class cleverbot:
         core.addCommandHandler("cleverbot", self, chelp="cleverbot.help",
             alias=['cb'])
         self.chans = {}
+        factory = ChatterBotFactory()
+        self.cleverbot = factory.create(ChatterBotType.CLEVERBOT)
 
     def cleverbot(self, bot, cli, ev):
         if len(ev.splitd) == 0:
@@ -19,113 +17,193 @@ class cleverbot:
             self.chans[ev.target]
             if (time.time() - self.chans[ev.target]['ts']) > 1800:
                 del self.chans[ev.target]['bot']
-                self.chans[ev.target]['bot'] = Session()
+                self.chans[ev.target]['bot'] = self.cleverbot.create_session()
         except:
             self.chans[ev.target] = {}
-            self.chans[ev.target]['bot'] = Session()
+            self.chans[ev.target]['bot'] = self.cleverbot.create_session()
         self.chans[ev.target]['ts'] = time.time()
         s = " ".join(ev.splitd)
-        cli.msg(ev.target, self.chans[ev.target]['bot'].Ask(s))
+        cli.msg(ev.target, self.chans[ev.target]['bot'].think(s))
 
 
-class ServerFullError(Exception):
-        pass
+import md5
+import urllib.request
+import urllib.parse
+import urllib.error
+import uuid
+import xml.dom.minidom
 
-ReplyFlagsRE = re.compile('<INPUT NAME=(.+?) TYPE=(.+?) VALUE="(.*?)">',
-                                                 re.IGNORECASE | re.MULTILINE)
+"""
+    chatterbotapi
+    Copyright (C) 2011 pierredavidbelanger@gmail.com
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-class Session(object):
-        keylist = ['stimulus', 'start', 'sessionid', 'vText8', 'vText7',
-                'vText6', 'vText5', 'vText4', 'vText3', 'vText2', 'icognoid',
-                'icognocheck', 'prevref', 'emotionaloutput', 'emotionalhistory',
-                'asbotname', 'ttsvoice', 'typing', 'lineref', 'fno', 'sub',
-                'islearning', 'cleanslate']
-        headers = {}
-        headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:7.0.1)"\
-                                " Gecko/20100101 Firefox/7.0'
-        headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;" \
-                            "q=0.9,*/*;q=0.8'
-        headers['Accept-Language'] = 'en-us;q=0.8,en;q=0.5'
-        headers['X-Moz'] = 'prefetch'
-        headers['Accept-Charset'] = 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'
-        headers['Referer'] = 'http://www.cleverbot.com'
-        headers['Cache-Control'] = 'no-cache, no-cache'
-        headers['Pragma'] = 'no-cache'
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
 
-        def __init__(self):
-                self.arglist = ['', 'y', '', '', '', '', '', '', '', '', 'wsf',
-                    '', '', '', '', '', '', '', '', '0', 'Say', '1', 'false']
-                self.MsgList = []
-
-        def Send(self):
-                data = encode(self.keylist, self.arglist)
-                digest_txt = data[9:35].encode('utf-8')
-                hash2 = hashlib.md5(digest_txt).hexdigest()
-                self.arglist[self.keylist.index('icognocheck')] = hash2
-                data = encode(self.keylist, self.arglist)
-                binary_data = data.encode('utf-8')
-                with urllib.request.urlopen("http://www.cleverbot.com/webservi"
-                                            "cemin", binary_data, 5000) as url:
-                    reply = url.read()
-                return reply
-
-        def Ask(self, q):
-                self.arglist[self.keylist.index('stimulus')] = q
-                if self.MsgList:
-                    self.arglist[self.keylist.index('lineref')] = '!0' + \
-                                                str(len(self.MsgList) / 2)
-                asw = self.Send()
-                self.MsgList.append(q)
-                answer = parseAnswers(asw)
-                for k, v in list(answer.items()):
-                        try:
-                                self.arglist[self.keylist.index(k)] = v
-                        except ValueError:
-                                pass
-                self.arglist[self.keylist.index('emotionaloutput')] = ''
-                text = answer['ttsText']
-                self.MsgList.append(text)
-                h = html.parser.HTMLParser()
-                text = h.unescape(text)
-                return text
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 
-def parseAnswers(text):
-        d = {}
-        keys = ["text", "sessionid", "logurl", "vText8", "vText7", "vText6",
-                "vText5", "vText4", "vText3", "vText2", "prevref", "foo",
-                "emotionalhistory", "ttsLocMP3", "ttsLocTXT", "ttsLocTXT3",
-                "ttsText", "lineRef", "lineURL", "linePOST", "lineChoices",
-                        "lineChoicesAbbrev", "typingData", "divert"]
-        text = str(text, 'utf-8')
-        values = text.split("\r")
-        i = 0
-        for key in keys:
-                d[key] = values[i]
-                i += 1
-        return d
+#################################################
+# API
+#################################################
+
+class ChatterBotType:
+
+    CLEVERBOT = 1
+    JABBERWACKY = 2
+    PANDORABOTS = 3
 
 
-def encode(keylist, arglist):
-        text = ''
-        for i in range(len(keylist)):
-                k = keylist[i]
-                v = quote(arglist[i])
-                text += '&' + k + '=' + v
-        text = text[1:]
-        return text
+class ChatterBotFactory:
 
-always_safe = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-               'abcdefghijklmnopqrstuvwxyz'
-               '0123456789' '_.-')
+    def create(self, type, arg=None):
+        if type == ChatterBotType.CLEVERBOT:
+            return _Cleverbot('http://www.cleverbot.com/webservicemin', 35)
+        elif type == ChatterBotType.JABBERWACKY:
+            return _Cleverbot('http://jabberwacky.com/webservicemin', 29)
+        elif type == ChatterBotType.PANDORABOTS:
+            if arg is None:
+                raise Exception('PANDORABOTS needs a botid arg')
+            return _Pandorabots(arg)
+        return None
 
 
-def quote(s, safe='/'):  # quote('abc def') -> 'abc%20def'
-        safe += always_safe
-        safe_map = {}
-        for i in range(256):
-                c = chr(i)
-                safe_map[c] = (c in safe) and c or ('%%%02X' % i)
-        res = list(map(safe_map.__getitem__, s))
-        return ''.join(res)
+class ChatterBot:
+
+    def create_session(self):
+        return None
+
+
+class ChatterBotSession:
+
+    def think_thought(self, thought):
+        return thought
+
+    def think(self, text):
+        thought = ChatterBotThought()
+        thought.text = text
+        return self.think_thought(thought).text
+
+
+class ChatterBotThought:
+
+    pass
+
+#################################################
+# Cleverbot impl
+#################################################
+
+
+class _Cleverbot(ChatterBot):
+
+    def __init__(self, url, endIndex):
+        self.url = url
+        self.endIndex = endIndex
+
+    def create_session(self):
+        return _CleverbotSession(self)
+
+
+class _CleverbotSession(ChatterBotSession):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.vars = {}
+        self.vars['start'] = 'y'
+        self.vars['icognoid'] = 'wsf'
+        self.vars['fno'] = '0'
+        self.vars['sub'] = 'Say'
+        self.vars['islearning'] = '1'
+        self.vars['cleanslate'] = 'false'
+
+    def think_thought(self, thought):
+        self.vars['stimulus'] = thought.text
+        data = urllib.parse.urlencode(self.vars)
+        data_to_digest = data[9:self.bot.endIndex]
+        data_digest = md5.new(data_to_digest).hexdigest()
+        data = data + '&icognocheck=' + data_digest
+        url_response = urllib.request.urlopen(self.bot.url, data)
+        response = url_response.read()
+        response_values = response.split('\r')
+        #self.vars['??'] = _utils_string_at_index(response_values, 0)
+        self.vars['sessionid'] = _utils_string_at_index(response_values, 1)
+        self.vars['logurl'] = _utils_string_at_index(response_values, 2)
+        self.vars['vText8'] = _utils_string_at_index(response_values, 3)
+        self.vars['vText7'] = _utils_string_at_index(response_values, 4)
+        self.vars['vText6'] = _utils_string_at_index(response_values, 5)
+        self.vars['vText5'] = _utils_string_at_index(response_values, 6)
+        self.vars['vText4'] = _utils_string_at_index(response_values, 7)
+        self.vars['vText3'] = _utils_string_at_index(response_values, 8)
+        self.vars['vText2'] = _utils_string_at_index(response_values, 9)
+        self.vars['prevref'] = _utils_string_at_index(response_values, 10)
+        #self.vars['??'] = _utils_string_at_index(response_values, 11)
+        self.vars['emotionalhistory'] = _utils_string_at_index(response_values,
+                                                                             12)
+        self.vars['ttsLocMP3'] = _utils_string_at_index(response_values, 13)
+        self.vars['ttsLocTXT'] = _utils_string_at_index(response_values, 14)
+        self.vars['ttsLocTXT3'] = _utils_string_at_index(response_values, 15)
+        self.vars['ttsText'] = _utils_string_at_index(response_values, 16)
+        self.vars['lineRef'] = _utils_string_at_index(response_values, 17)
+        self.vars['lineURL'] = _utils_string_at_index(response_values, 18)
+        self.vars['linePOST'] = _utils_string_at_index(response_values, 19)
+        self.vars['lineChoices'] = _utils_string_at_index(response_values, 20)
+        self.vars['lineChoicesAbbrev'] = _utils_string_at_index(response_values,
+                                                                             21)
+        self.vars['typingData'] = _utils_string_at_index(response_values, 22)
+        self.vars['divert'] = _utils_string_at_index(response_values, 23)
+        response_thought = ChatterBotThought()
+        response_thought.text = _utils_string_at_index(response_values, 16)
+        return response_thought
+
+
+#################################################
+# Pandorabots impl
+#################################################
+
+class _Pandorabots(ChatterBot):
+
+    def __init__(self, botid):
+        self.botid = botid
+
+    def create_session(self):
+        return _PandorabotsSession(self)
+
+
+class _PandorabotsSession(ChatterBotSession):
+
+    def __init__(self, bot):
+        self.vars = {}
+        self.vars['botid'] = bot.botid
+        self.vars['custid'] = uuid.uuid1()
+
+    def think_thought(self, thought):
+        self.vars['input'] = thought.text
+        data = urllib.parse.urlencode(self.vars)
+        url_response = urllib.request.urlopen('http://www.pandorabots.com/pand'
+                                                        'ora/talk-xml', data)
+        response = url_response.read()
+        response_dom = xml.dom.minidom.parseString(response)
+        response_thought = ChatterBotThought()
+        response_thought.text = response_dom.getElementsByTagName('that')[0] \
+                                        .childNodes[0].data.strip()
+        return response_thought
+
+#################################################
+# Utils
+#################################################
+
+
+def _utils_string_at_index(strings, index):
+    if len(strings) > index:
+        return strings[index]
+    else:
+        return ''
