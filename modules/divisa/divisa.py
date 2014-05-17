6962bb99424a3e2c0e45a39a4aac9d1f3f61caa7
Ramiro Bou <bouramiro@gmail.com>
2014-05-17 02:31:43 -0300

    Teniendo en cuenta la cantidad en m_divisa.

--- modules/divisa/divisa.py
+++ modules/divisa/divisa.py
@@ -39,5 +39,5 @@ class divisa:
              bot.readConf("config.openexchratesapikey")).read().decode()
             self.thing = json.loads(r)
             self.thingtime = time.time()
-        cli.msg(ev.target, bot._(ev, self, 'convmsg').format(dfrom, dto, kfrom / kto))
+        cli.msg(ev.target, bot._(ev, self, 'convmsg').format(dfrom, dto, (kfrom / kto) * ev.splitd[2]))
         
