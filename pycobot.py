0bb825dc2ffcc9359beec5c4b7496369a810167d
Ramiro Bou <bouramiro@gmail.com>
2014-05-16 21:17:51 -0300

    Dejando de usar tmp/

--- pycobot.py
+++ pycobot.py
@@ -4,8 +4,6 @@
 from irc.client import ClientPool
 import sys
 import logging
-import shutil
-import os
 from pycobot.pycobot import pyCoBot
 from pycobot.kaptan import Kaptan
 #from pprint import pprint
@@ -33,28 +31,12 @@ def main():
         raise ValueError('Invalid log level: %s' % loglevel)
     logging.basicConfig(level=numeric_level,
         filename=conf.get("config.logfile"), filemode='w')
-    # Al iniciar borramos todo lo que hay en tmp/
-    folder = 'tmp'
-    for the_file in os.listdir(folder):
-        file_path = os.path.join(folder, the_file)
-        try:
-            if not file_path == "tmp/__init__.py":
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-                else:
-                    shutil.rmtree(file_path)
-        except Exception:
-            pass
 
     client = ClientPool()
 
     # Añadir servidores
     for i, val in enumerate(conf.get("irc")):
         #conf['irc'][i]['pserver'] = conf['irc'][i]['server'].replace(".", "")
-        if not os.path.exists("tmp/" + val):
-            os.makedirs("tmp/" + val)
-            l = open("tmp/" + val + "/__init__.py", "w")
-            l.write("# :P")  # Con un __init__.py
         servers.append(pyCoBot(conf.get("irc.{0}.server".format(val)), client,
          conf.get("irc.{0}".format(val)), conf, val))
     client.boservers = servers
