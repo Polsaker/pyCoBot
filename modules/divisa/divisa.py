265fcbb5b16b1d1148afb59e3f8d34a7529e4564
Ramiro Bou <bouramiro@gmail.com>
2014-05-17 02:10:25 -0300

    Añdiendo módulo divisa

---
+++ modules/divisa/divisa.py
@@ -0,0 +1,37 @@
+import time
+import json
+
+
+class divisa:
+    def __init__(self, core, client):
+        core.addCommandHandler("conv", self, chelp="conv.help")
+        self.thing = None
+        self.thingtime = 0
+    
+    def conv(self, bot, cli, ev):
+        if len(ev.splitd) < 2:
+            cli.msg(ev.target, bot._(ev, 'core', "generic.missigparam"))
+            return 1
+        dfrom = ev.splitd[0].upper()
+        dto = ev.splitd[1].upper()
+        try:
+            kfrom = self.thing['rates'][dfrom]
+        except:
+            cli.msg(ev.target, bot._(ev, self, 'err.invaliddiv').format(dfrom))
+            return 1
+        try:
+            kto = self.thing['rates'][dto]
+        except:
+            cli.msg(ev.target, bot._(ev, self, 'err.invaliddiv').format(dto))
+            return 1
+        dfrom = bot._(ev, self, dfrom)
+        dto = bot._(ev, self, dto)
+            
+        if (time.time() - self.thigtime) < 1800:
+            r = urllib.request.urlopen("http://openexchangerate"
+             "s.org/api/latest.json?app_id=" +
+             bot.readConf("config.openexchratesapikey")).read().decode()
+            self.thing = json.loads(r)
+            self.thingtime = time.time()
+        cli.msg(ev.target, bot._(ev, self, 'convmsg').format(dfrom, dto, kfrom / kto))
+        
