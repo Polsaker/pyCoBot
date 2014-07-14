f6d4928e34aca24fd5dccc06f86668033d531dfd
Ramiro Bou <bouramiro@gmail.com>
2014-07-14 03:01:05 -0300

    Añadiendo una lista de los numéricos del irc.

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 pyCoBot
 =======
-
-pyCoBot es un bot IRC escrito en Python y basado en CoBot.
-
-Instalación
-===========
-
-1. Copiar el archivo `pycobot.conf.example` a `pycobot.conf`
-2. [Editar pycobot.conf](https://github.com/irc-CoBot/pyCoBot/wiki/Configuración)
-3. `python3 pycobot.py` (Al arrancarlo por primera vez le pedirá que introduzca un usuario y una contraseña para crear un usuario administrador)
-4. ...
-5. Profit!
--- pycobot/__init__.py
+++ pycobot/__init__.py
@@ -1,1 +1,33 @@
 # -*- coding: utf-8 -*-
+
+from pycobot import daemon
+import sys
+from pycobot.kaptan import Kaptan
+import logging
+
+VERSION = "2.1"
+CODENAME = "Dors"
+
+# :D
+logging.basicConfig(format="%(asctime)s: %(name)s: %(levelname)s (at %(filename"
+                    ")s:%(funcName)s:%(lineno)d): %(message)s")
+
+
+class bot(daemon.Daemon):
+    config = None
+
+    def __init__(self, pid="/var/tmp/pycobot.pid"):
+        super(bot, self).__init__(pid, stdout="pycobot.log",
+                                       stderr="pycobot.err")
+
+        try:
+            jsonConf = open("pycobot.conf").read()
+        except IOError:
+            logging.critical('No se ha podido abrir el archivo de'
+                          ' configuración')
+            sys.exit("Missing config file!")
+        config = Kaptan(handler="json")
+        config.import_config(jsonConf)
+
+    def run(self):
+        pass
