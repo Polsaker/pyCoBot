a5af7608b2369aae613196536956e9e87305568c
Ramiro Bou <buramro@gmail.com>
2014-09-02 00:52:16 -0300

    Ooops! Mostrando correctamente el título de los libros.

--- modules/books/books.py
+++ modules/books/books.py
@@ -126,10 +126,9 @@ class books:
             return
 
         for b in search['items']:
-            resp = "\002{0}\002, "
+            resp = "\002{0}\002, ".format(b['volumeInfo']['title'])
             try:
-                resp = "Autor(es): \002{1}\002. ".format(
-                    b['volumeInfo']['title'], ", ".join(b['volumeInfo']['authors']))
+                resp = "Autor(es): \002{1}\002. ".format(", ".join(b['volumeInfo']['authors']))
             except:
                 pass
             try:
