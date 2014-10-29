8e729634c5c8dde851913ded80d445b8e7e87ea5
Polsaker <bouramiro@gmail.com>
2014-10-28 22:22:53 -0300

    Arreglado el módulo games cuando se usa el prefijo "!"

--- modules/games/games.py
+++ modules/games/games.py
@@ -193,7 +193,7 @@ class games:
 
     def commandhandle(self, cli, event):
         ev = copy.deepcopy(event)
-        if not ev.splitd[0][0] == "!":
+        if not ev.arguments[0][0] == "!":
             return 0
         c = GameChannel.get(GameChannel.channel == ev.target)
         if c is False:
