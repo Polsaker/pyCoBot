77f7e84c91ce6a98aa0c3a9efb4d40905104e98d
Ramiro Bou <bouramiro@gmail.com>
2014-11-23 21:06:22 -0300

    Making more progress on the commands

--- pycobot/network.py
+++ pycobot/network.py
@@ -7,10 +7,11 @@
 import json
 import logging
 from .irc import client
-import pycobot.database
+from .database import Settings
 import imp
 import sys
 import inspect
+import ast
 
 class Server:
     config = None
@@ -31,6 +32,8 @@ class Server:
         self.sid = sid
 
         self.connection = client.IRCClient(sid)
+        self.connection.addhandler("privmsg", self._processCommands)
+        self.connection.addhandler("pubmsg", self._processCommands)
 
         sconf = self.config.get("servers.{0}".format(sid))
         self.connection.configure(sconf['host'], sconf['port'], sconf['nick'])
@@ -43,12 +46,53 @@ class Server:
 
         self.connection.connect()
         
+    def _processCommands(self, conn, event):
+        command = None
+        if event.type == "privmsg":
+            cmd = event.splitd[0]
+        else:
+            try:
+                prefix = self.getSetting("prefix", event.source)
+            except:
+                pass  # No prefix ?!?!?!
+            
+            if type(prefix) is list:
+                for i in prefix: # :(
+                    if event.splitd[0][:len(i)] == i:
+                        command = event.splitd[0][len(i):]
+                        break
+            else:
+                if event.splitd[0][:len(prefix)] == i:
+                    command = event.splitd[0][len(prefix):]
+        
+        if not command:
+            return
+        
+        # Valid order prefix for the bot! Check if the command exists
+        if command == "auth" or command == "id" or command == "identify":
+            # TODO: Core command 'auth'
+            return
+        elif command == "help" or command == "ayuda": # TODO: command i18n?
+            # TODO: help command
+            return
+            
+        try:
+            self.commands[command]
+        except:
+            return # 404 command not found
+        
+        if self.commands[command]['privs'] != 0:
+            # TODO: Command privilege system
+            return
+        
+        # Call the function
+        self.commands[command]['func'](self, self.connection, event)
+        
     def _autojoin(self, conn, event):
         # TODO: Also read the database for autojoins
         for chan in self.config.get("servers.{0}.autojoin".format(self.sid)):
             conn.join(chan)
             
-    
     # Loads a module locally.
     # Returns:
     #  -1: Module not found
@@ -119,7 +163,47 @@ class Server:
         # TODO: Finish user/privs system to make commands work
     
     def getSetting(self, key, channel=None):
-        pass  # TODO: Read settings
+        # 1 - Try to read channel config
+        if channel is not "1":
+            try:
+                s = Settings.get(Settings.channel == channel, Settings.network == self.sid,
+                    Settings.type == "channel", Settings.name == key)
+                try:
+                    s = ast.literal_eval(s.value)
+                except:
+                    pass
+                return s.value
+            except:
+                pass
+        # 2 - Try to read network config
+        try:
+            s = Settings.get(Settings.network == self.sid,
+                    Settings.type == "network", Settings.name == key)
+            try:
+                s = ast.literal_eval(s.value)
+            except:
+                pass
+            return s.value
+        except:
+            pass
+        # 3 - Try to read the global config
+        try:
+            s = Settings.get(Settings.type == "global", Settings.name == key)
+            try:
+                s = ast.literal_eval(s.value)
+            except:
+                pass
+            return s.value
+        except:
+            pass
+        
+        # 4 - Try to read the config from the file
+        try:
+            self.config.get("servers.{0}.{1}".format(self.sid, key))
+        except:
+            raise  # Whoops! value not found
+                            
+        
 
     # Register a handler to be proxied
     # !!!! REGISTER YOUR HANDLERS HERE IF YOU DO WITHOUT DECORATORS !!!!
@@ -157,16 +241,6 @@ class Server:
                     self.logger.warning("Exception when calling handler"
                         " for module '{0}' (event: '{1}'): {2}".format(
                         module, event.type, e))
-
-    # Reads values from the config file DEPRECATED BY getSetting!
-    def readConf(self, key, chan=None, default=""):
-        key = key.replace("network", "irc." + str(self.sid))
-        if chan is not None:
-            key = key.replace("channel.", "irc." + str(self.sid) + ".channels."
-                                                        + chan.lower() + ".")
-            if key == "channel":
-                key = "irc." + str(self.sid) + ".channels." + chan.lower()
-        return self.config.get(key, default)
 
 
     # Deprecated!
